efbfc1cd1f6472961809b074d4f0bbed0596fdb8
pedro z brunhara <pedrozbrunhara@usp.br>
2023-11-26 15:55:22 -0300

    commit

--- trabSO2.c
+++ trabSO2.c
@@ -11,14 +11,14 @@ typedef struct psg{
     int pontoVaiSubir;              //    -1 = ponto de onibus inicial
     int pontoVaiDescer;             //    0  = onibus 0
     int ondeEstou;                  //    1  = onibus 1
-                                    //    n  = onibus n
+    int flagEspera;                 //    n  = onibus n
                                     //    -2 = chegou no destino
 }passageiro;
 
 typedef struct obs{                 //OndeEstou:
     int id;                         //    0  = ponto de onibus 0 
-    sem_t acentosLivres;            //    n  = ponto de onibus n
-    int ondeEstou;                  //    -1 = se locomovendo entre pontos                
+    int ondeEstou;                  //    n  = ponto de onibus n
+    int acetosLivres;              //    -1 = se locomovendo entre pontos 
 }onibusSTC;
 /*
 Como nos exemplos do livro Sistemas Operacionais Modernos presentes nas figuras
@@ -30,101 +30,88 @@ int idPassag    = 0;
 int processados = 0;
 int idOnibus    = 0;
 
-sem_t semaforoPassageiro[MAX];
-sem_t multex1,multex2,multex3;
+sem_t semaforoPontoDeOnibus[MAX];
+sem_t multex1,multex2,multex3,multex4,multex5;
 passageiro listaPassageiros[MAX];
 onibusSTC onibus[MAX]; 
 
-passageiro produzPassageiro(){
-    passageiro pas;
-    pas.pontoVaiSubir = rand() % s;
-    pas.pontoVaiDescer= rand() % s;
-    pas.ondeEstou     = -1;
-    sem_wait(&multex2); 
-    pas.id = idPassag;
-    idPassag++;
-    sem_post(&multex2);
-    //printf("pessoa id:%d inicio:%d fim:%d \n",pas.id,pas.pontoVaiSubir,pas.pontoVaiDescer);
-    return pas;
-}
-
 void* funcPassageiro(void *arg){    // responsavel por produzir os passageiros
     //pessageiro é criado e inserido nos pontos
-    passageiro pas;
+    int id;
 
-    sem_wait(&multex1); 
-    pas= produzPassageiro();        //Passageiro é produzido
-    listaPassageiros[pas.id]=pas;   //Passageiro é incerido nos postos
+    sem_wait(&multex2); 
+    id = idPassag;
+    idPassag++;
+    sem_post(&multex2);
+
+    sem_wait(&multex1);
+    listaPassageiros[id].id = id;
+    listaPassageiros[id].pontoVaiSubir = rand() % s;
+    listaPassageiros[id].pontoVaiDescer= rand() % s;
+    listaPassageiros[id].ondeEstou = -1;
+    listaPassageiros[id].flagEspera = 0;
     sem_post(&multex1);
     
     while (1)
     {
-        if(sem_trywait(&semaforoPassageiro[pas.id])!= -1) break;
+        if(listaPassageiros[id].flagEspera) break;
     }
-    
-
-
     
     int aux;
     while (1)
     {
-        if(listaPassageiros[pas.id].pontoVaiDescer == onibus[listaPassageiros[pas.id].ondeEstou].ondeEstou){
-            aux = listaPassageiros[pas.id].ondeEstou;
+        if(listaPassageiros[id].pontoVaiDescer == onibus[listaPassageiros[id].ondeEstou].ondeEstou){
+            aux = listaPassageiros[id].ondeEstou;
             sem_wait(&multex1);
-            listaPassageiros[pas.id].ondeEstou = -2;
-            printf("passageiro %d desceu do onibus %d no pontpo %d\n",
-            listaPassageiros[pas.id].id,
-            onibus[aux].id,
-            listaPassageiros[pas.id].pontoVaiDescer);
+            listaPassageiros[id].ondeEstou = -2;
+            sem_wait(&multex4);
+            onibus[aux].acetosLivres++;
+            processados++;
+            printf("processados: %d\n",processados);
+            printf("+++Fim thread do passageiro %d\n", id);
+            sem_post(&multex4);
             sem_post(&multex1);
-            sem_post(&onibus[aux].acentosLivres);printf("semaforo onibus %d ++\n",
-            onibus[aux].id);
             break;
         }
     }
-    /*
-    printf("---------------------------\n");
-    for(int i = 0; i<p;i++){
-        printf("|id: %d local: %d|\n",listaPassageiros[i].id,listaPassageiros[i].ondeEstou);
-    }printf("---------------------------\n");
-    */
-
-    printf("+++Fim thread do passageiro %d\n", pas.id);
     pthread_exit(NULL);
 }
 
 void* funcaoOnibus(void* arg){
     int aux = rand();
     int esseOnibus;
-    //onibus onibus;
+    
     sem_wait(&multex3);
     esseOnibus= idOnibus;
     idOnibus++;
-    printf("*********thread do onibus %d\n", esseOnibus);
     sem_post(&multex3);  
+    
     onibus[esseOnibus].id= esseOnibus;
     onibus[esseOnibus].ondeEstou = aux % s;
-    sem_init(&onibus[esseOnibus].acentosLivres,0,a);
+    onibus[esseOnibus].acetosLivres=a;
     
-    while(processados<p){
+    while(1){
         aux++;
         onibus[esseOnibus].ondeEstou = aux % s;
-        if(sem_trywait(&arg[(aux%s)])){
+        if(sem_trywait(&semaforoPontoDeOnibus[(aux%s)])){
             for(int i=0; i<p ;i++){
-                if(listaPassageiros[i].ondeEstou == -1 && 
-                    listaPassageiros[i].pontoVaiSubir == onibus[esseOnibus].ondeEstou){
-                    sem_wait(&onibus[esseOnibus].acentosLivres);
-                    listaPassageiros[i].ondeEstou = onibus[esseOnibus].id;
-                    processados++;
-                    sem_post(&semaforoPassageiro[i]);
+                sem_wait(&multex5);
+                if(listaPassageiros[i].pontoVaiSubir == onibus[esseOnibus].ondeEstou &&
+                listaPassageiros[i].ondeEstou == -1 &&
+                onibus[esseOnibus].acetosLivres > 0){
+                    sem_wait(&multex1);
+                    listaPassageiros[i].flagEspera = 1;
+                    sem_post(&multex1);
+                    listaPassageiros[i].ondeEstou = esseOnibus;
+                    onibus[esseOnibus].acetosLivres --;
                     printf("passageiro %d embarcou no onibus %d\n",i,esseOnibus);
                 }
+                sem_post(&multex5);
             }
-            onibus[esseOnibus].ondeEstou = -2;
-            sem_post(&arg[aux%s]);       
-        }  
+            sem_post(&semaforoPontoDeOnibus[aux%s]);       
+        } 
+        if(processados >= p)break;
     }
-  
     printf("Fim do onibus %d\n", onibus[esseOnibus].id);
     pthread_exit(NULL);
 }
@@ -138,7 +125,10 @@ int main(){
     sem_init(&multex1,0,1);
     sem_init(&multex2,0,1);
     sem_init(&multex3,0,1);
-    
+    sem_init(&multex4,0,1);
+    sem_init(&multex5,0,1);
+    pthread_t onibus[c];
+    pthread_t passageiros[p];
 
 
 //    pthread_t pontoDeOnibus[s];
@@ -147,25 +137,12 @@ int main(){
         sem_init(&semaforoPontoDeOnibus[i],0,1);
     }
 
-    pthread_t onibus[c];
-    sem_t semaforoOnibus[c];
-    for(int i=0;i<c;i++){
-        sem_init(&semaforoOnibus[c],0,a);
-    }
-
-    pthread_t passageiros[p];
-    //sem_t semaforoPassageiro[p];
-    for(int i=0;i<p;i++){
-        sem_init(&semaforoPassageiro[i],0,0);
-    }
-
     int status;             
     //------------------------------------------------------------------------------------
     //Inicializando as Threads do Pontos de Onibus
 
     for(int i=0;i<p;i++){
-        printf("Metodo Main. Criando passageiro %d\n", i);
-        status =pthread_create(&passageiros[i], NULL, (void*)funcPassageiro, (void *) &semaforoPassageiro);
+        status =pthread_create(&passageiros[i], NULL, (void*)funcPassageiro, NULL);
         if (status != 0) {
             printf("Oops. pthread create returned error code %d\n", status);
             exit(-1);
@@ -178,7 +155,6 @@ int main(){
    //Inicializando as Threads do Onibus
 
     for(int i=0;i<c;i++){
-        printf("Metodo Main. Criando onibus %d\n", i);
         status =pthread_create(&onibus[i], NULL, (void*)funcaoOnibus,(void*) &semaforoPontoDeOnibus );
         if (status != 0) {
             printf("Oops. pthread create returned error code %d\n", status);
@@ -193,7 +169,6 @@ int main(){
     for(int i = 0; i<p;i++){
         printf("id: %d local: %d\n",listaPassageiros[i].id,listaPassageiros[i].ondeEstou);
     }
- 
 
     return 0;
 }
